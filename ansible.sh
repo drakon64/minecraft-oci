0d587967d23f16e93d9a8b0ef7ddafc8d77a9aa9
Adam Chance <adam.chance10@live.co.uk>
2021-05-30 13:45:35 +0100

    Support Java Edition in ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-ansible-playbook --diff --inventory "$1", --user ubuntu --become ansible/minecraft.yml --extra-vars edition=bedrock
+if [ -z "$1" ] ; then
+	echo "You must specify an IP address or DNS name"
+	exit 1
+fi
+
+if [ -z "$2" ] || [ "$2" != "bedrock" ] && [ "$2" != "java" ] ; then
+	echo "You must specify one of the following arguments: bedrock	java"
+	exit 1
+elif [ "$2" = "bedrock" ] ; then
+	ansible-playbook --diff --inventory "$1", --user ubuntu --become ansible/minecraft.yml --extra-vars edition=bedrock
+elif [ "$2" = "java" ] ; then
+	ansible-playbook --diff --inventory "$1", --user ubuntu --become ansible/minecraft.yml --extra-vars edition=java
+fi
