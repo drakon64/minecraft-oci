5f81a5dafbd954015bb5f6330030c93c90543ec7
Adam Chance <adam.chance10@live.co.uk>
2021-05-31 14:19:31 +0100

    Support downloading a server backup

--- ansible.sh
+++ ansible.sh
@@ -1,15 +1,54 @@
 #!/bin/sh
 
-if [ -z "$1" ] ; then
-	echo "You must specify an IP address or DNS name"
-	exit 1
-fi
+check_ip() {
+	if [ -z "$2" ] ; then
+		echo "You must specify an IP address or DNS name"
+		exit 1
+	fi
+}
 
-if [ -z "$2" ] || [ "$2" != "bedrock" ] && [ "$2" != "java" ] ; then
-	echo "You must specify one of the following arguments: bedrock	java"
-	exit 1
-elif [ "$2" = "bedrock" ] ; then
-	ansible-playbook --diff --inventory "$1", --user ubuntu --become ansible/minecraft.yml --extra-vars "edition=bedrock command='/home/minecraft/bedrock/bedrock_server'"
-elif [ "$2" = "java" ] ; then
-	ansible-playbook --diff --inventory "$1", --user ubuntu --become ansible/minecraft.yml --extra-vars "edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui'"
-fi
+check_edition() {
+	if [ -z "$3" ] || [ "$3" != "bedrock" ] && [ "$3" != "java" ] ; then
+		echo "You must specify one of the following arguments: bedrock	java"
+		exit 1
+	fi
+}
+
+while getopts :inbp arg ; do
+	case ${arg} in
+		i)
+			check_ip "$@"
+			check_edition "$@"
+
+			if [ "$3" = "bedrock" ] ; then
+				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=false restore_backup=false" > ansible/inventory
+			elif [ "$3" = "java" ] ; then
+				echo "$3 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' new_server=false restore_backup=false" > ansible/inventory
+			fi
+			;;
+		n)
+			check_ip "$@"
+			check_edition "$@"
+
+			if [ "$3" = "bedrock" ] ; then
+				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=true restore_backup=false" > ansible/inventory
+			elif [ "$3" = "java" ] ; then
+				echo "$3 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' new_server=true restore_backup=false" > ansible/inventory
+			fi
+			;;
+		b)
+			check_ip "$@"
+			check_edition "$@"
+
+			if [ "$3" = "bedrock" ] ; then
+				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=true restore_backup=true" > ansible/inventory
+			elif [ "$3" = "java" ] ; then
+				echo "$2 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' new_server=true restore_backup=true" > ansible/inventory
+			fi
+			;;
+		p)
+			ansible-playbook --diff --inventory ansible/inventory --user ubuntu --become ansible/minecraft.yml
+			;;
+		*)
+	esac
+done
--- oci/group.tf
+++ oci/group.tf
@@ -11,7 +11,7 @@ resource "oci_identity_policy" "minecraft_backup" {
 	description = "Minecraft backup"
 	name = "minecraft-backup"
 	statements = [
-		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft_backup.name} to read buckets in compartment ${oci_identity_compartment.tf-compartment.name}",
-		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft_backup.name} to manage objects in compartment ${oci_identity_compartment.tf-compartment.name} where any {request.permission='OBJECT_CREATE', request.permission='OBJECT_INSPECT'}"
+		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft_backup.name} to read buckets in compartment id ${oci_identity_compartment.tf-compartment.id} where target.bucket.name='${oci_objectstorage_bucket.minecraft_backup.name}'",
+		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft_backup.name} to manage objects in compartment id ${oci_identity_compartment.tf-compartment.id} where all {target.bucket.name='${oci_objectstorage_bucket.minecraft_backup.name}', any {request.permission='OBJECT_INSPECT', request.permission='OBJECT_READ', request.permission='OBJECT_CREATE'}}"
 	]
 }
