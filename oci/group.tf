35f9d55296bf0eb5f209344c04a0a78c1c8bc96e
Adam Chance <adam.chance10@live.co.uk>
2021-06-01 09:06:53 +0100

    Fix uploading a backup when one already exists

--- oci/group.tf
+++ oci/group.tf
@@ -3,7 +3,7 @@ resource "oci_identity_dynamic_group" "minecraft_backup" {
 	description = "Minecraft backup"
 	name = "minecraft-backup"
 
-	matching_rule = "All {instance.id = '${oci_core_instance.minecraft_instance.id}'}"
+	matching_rule = "instance.id = '${oci_core_instance.minecraft_instance.id}'"
 }
 
 resource "oci_identity_policy" "minecraft_backup" {
@@ -12,6 +12,6 @@ resource "oci_identity_policy" "minecraft_backup" {
 	name = "minecraft-backup"
 	statements = [
 		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft_backup.name} to read buckets in compartment id ${oci_identity_compartment.tf-compartment.id} where target.bucket.name='${oci_objectstorage_bucket.minecraft_backup.name}'",
-		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft_backup.name} to manage objects in compartment id ${oci_identity_compartment.tf-compartment.id} where all {target.bucket.name='${oci_objectstorage_bucket.minecraft_backup.name}', any {request.permission='OBJECT_INSPECT', request.permission='OBJECT_READ', request.permission='OBJECT_CREATE'}}"
+		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft_backup.name} to manage objects in compartment id ${oci_identity_compartment.tf-compartment.id} where all {target.bucket.name='${oci_objectstorage_bucket.minecraft_backup.name}', any {request.permission='OBJECT_INSPECT', request.permission='OBJECT_READ', request.permission='OBJECT_CREATE', request.permission='OBJECT_OVERWRITE'}}"
 	]
 }
