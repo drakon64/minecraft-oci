04259c8a7fe88121b1319a1ba512348a752c278c
Adam Chance <adam.chance10@live.co.uk>
2021-06-03 10:17:55 +0100

    Fix creating the Ansible inventory in some cases

--- ansible.sh
+++ ansible.sh
@@ -23,7 +23,7 @@ while getopts :inbp arg ; do
 			if [ "$3" = "bedrock" ] ; then
 				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=false restore_backup=false" > ansible/inventory
 			elif [ "$3" = "java" ] ; then
-				echo "$3 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' new_server=false restore_backup=false" > ansible/inventory
+				echo "$2 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' new_server=false restore_backup=false" > ansible/inventory
 			fi
 			;;
 		n)
@@ -33,7 +33,7 @@ while getopts :inbp arg ; do
 			if [ "$3" = "bedrock" ] ; then
 				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=true restore_backup=false" > ansible/inventory
 			elif [ "$3" = "java" ] ; then
-				echo "$3 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' new_server=true restore_backup=false" > ansible/inventory
+				echo "$2 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' new_server=true restore_backup=false" > ansible/inventory
 			fi
 			;;
 		b)
