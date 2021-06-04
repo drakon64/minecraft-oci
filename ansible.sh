521d5c911955f23b5354aa63d3d6627df82e352c
Adam Chance <adam.chance10@live.co.uk>
2021-06-04 17:59:41 +0100

    Allow using Paper/Geyser when restoring a backup

--- ansible.sh
+++ ansible.sh
@@ -50,7 +50,7 @@ while getopts :inbp arg ; do
 			if [ "$3" = "bedrock" ] ; then
 				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=true restore_backup=true" > ansible/inventory
 			elif [ "$3" = "java" ] || [ "$3" = "paper" ] || [ "$3" = "geyser" ] ; then
-				echo "$2 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=true" > ansible/inventory
+				echo "$2 edition=$3 command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=true" > ansible/inventory
 			else
 				echo "You must specify one of the following arguments: bedrock	java	paper	geyser"
 				exit 1
