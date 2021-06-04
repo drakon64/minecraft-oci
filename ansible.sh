b79ce3b22371be88c89027615c2ce4bdb42b1157
Adam Chance <adam.chance10@live.co.uk>
2021-06-04 17:57:31 +0100

    Cleanup ansibe.sh

--- ansible.sh
+++ ansible.sh
@@ -7,13 +7,6 @@ check_ip() {
 	fi
 }
 
-check_edition() {
-	if [ -z "$3" ] || [ "$3" != "bedrock" ] && [ "$3" != "java" ] && [ "$3" != "paper" ] && [ "$3" != "geyser" ] ; then
-		echo "You must specify one of the following arguments: bedrock	java	paper	geyser"
-		exit 1
-	fi
-}
-
 check_eula() {
 	if [ -z "$4" ] || [ "$4" = "no" ] || [ "$4" = "false" ] ; then
 		EULA="false"
@@ -21,6 +14,7 @@ check_eula() {
 		EULA="true"
 	else
 		echo "You must specify one of the following arguments: yes	no"
+		exit 1
 	fi
 }
 
@@ -28,47 +22,38 @@ while getopts :inbp arg ; do
 	case ${arg} in
 		i)
 			check_ip "$@"
-			check_edition "$@"
 			check_eula "$@"
 
 			if [ "$3" = "bedrock" ] ; then
 				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=false restore_backup=false" > ansible/inventory
-			elif [ "$3" = "java" ] ; then
-				echo "$2 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=false restore_backup=false" > ansible/inventory
-			elif [ "$3" = "paper" ] ; then
-				echo "$2 edition=paper java=openjdk-16-jre-headless command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=false restore_backup=false" > ansible/inventory
-			elif [ "$3" = "geyser" ] ; then
-				echo "$2 edition=geyser java=openjdk-16-jre-headless command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=false restore_backup=false" > ansible/inventory
+			elif [ "$3" = "java" ] || [ "$3" = "paper" ] || [ "$3" = "geyser" ] ; then
+				echo "$2 edition=$3 command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=false restore_backup=false" > ansible/inventory
 			fi
 			;;
 		n)
 			check_ip "$@"
-			check_edition "$@"
 			check_eula "$@"
 
 			if [ "$3" = "bedrock" ] ; then
 				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=true restore_backup=false" > ansible/inventory
-			elif [ "$3" = "java" ] ; then
-				echo "$2 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=false" > ansible/inventory
-			elif [ "$3" = "paper" ] ; then
-				echo "$2 edition=paper java=openjdk-16-jre-headless command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=false" > ansible/inventory
-			elif [ "$3" = "geyser" ] ; then
-				echo "$2 edition=geyser java=openjdk-16-jre-headless command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=false" > ansible/inventory
+			elif [ "$3" = "java" ] || [ "$3" = "paper" ] || [ "$3" = "geyser" ] ; then
+				echo "$2 edition=$3 command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=false" > ansible/inventory
+			else
+				echo "You must specify one of the following arguments: bedrock	java	paper	geyser"
+				exit 1
 			fi
 			;;
 		b)
 			check_ip "$@"
-			check_edition "$@"
 			check_eula "$@"
 
 			if [ "$3" = "bedrock" ] ; then
 				echo "$2 edition=bedrock command='/home/minecraft/bedrock/bedrock_server' new_server=true restore_backup=true" > ansible/inventory
-			elif [ "$3" = "java" ] ; then
+			elif [ "$3" = "java" ] || [ "$3" = "paper" ] || [ "$3" = "geyser" ] ; then
 				echo "$2 edition=java command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=true" > ansible/inventory
-			elif [ "$3" = "paper" ] ; then
-				echo "$2 edition=paper command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=true" > ansible/inventory
-			elif [ "$3" = "geyser" ] ; then
-				echo "$2 edition=geyser command='/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui' eula=$EULA new_server=true restore_backup=true" > ansible/inventory
+			else
+				echo "You must specify one of the following arguments: bedrock	java	paper	geyser"
+				exit 1
 			fi
 			;;
 		p)
