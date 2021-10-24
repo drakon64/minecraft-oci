18c21b67a5ed18a5a8e8d834c19e1c9307f50d94
Adam Chance <adam.chance10@live.co.uk>
2021-10-24 17:20:23 +0100

    Remove BedrockConnect

--- oci/bedrockconnect/compartment.tf
+++
@@ -1,5 +0,0 @@
-resource "oci_identity_compartment" "tf-compartment" {
-	compartment_id = var.oci_compartment
-	description = "BedrockConnect"
-	name = var.oci_compartment_name
-}
--- oci/bedrockconnect/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bedrockconnect_public_ip_address" {
-	value = oci_core_public_ip.bedrockconnect_public_ip.ip_address
-}
