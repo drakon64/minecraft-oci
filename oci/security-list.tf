91c434482a6d14918d6a4969fec99d89c2fc6993
Adam Chance <adam.chance10@live.co.uk>
2021-11-08 15:20:09 +0000

    Fix security list creation

--- oci/security-list.tf
+++ oci/security-list.tf
@@ -1,14 +1,14 @@
 resource "oci_core_default_security_list" "default-security-list" {
 	manage_default_resource_id = oci_core_vcn.vcn.default_security_list_id
 
-	ingress_security_rules {
+	egress_security_rules {
 		protocol = "all"
-		source = "0.0.0.0/0"
-		source_type = "CIDR_BLOCK"
+		destination = "0.0.0.0/0"
+		destination_type = "CIDR_BLOCK"
 		stateless = false
 	}
 
-	egress_security_rules {
+	ingress_security_rules {
 		protocol = "all"
 		source = "0.0.0.0/0"
 		source_type = "CIDR_BLOCK"
