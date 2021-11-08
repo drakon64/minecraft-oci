fef227303655e3280eb7468db6ca7689fd4368b1
Adam Chance <adam.chance10@live.co.uk>
2021-11-08 15:20:19 +0000

    Output the public IP address of the load balancer

--- oci/outputs.tf
+++ oci/outputs.tf
@@ -1,3 +1,7 @@
 output "minecraft_public_ip_address" {
 	value = oci_core_public_ip.minecraft_public_ip.ip_address
 }
+
+output "bluemap_public_ip_address" {
+	value = oci_load_balancer_load_balancer.bluemap.ip_address_details[0].ip_address
+}
