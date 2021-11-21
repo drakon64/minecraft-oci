5497b5022fa266f35f7b933293a0434589528fb1
Adam Chance <adam.chance10@live.co.uk>
2021-11-21 13:17:29 +0000

    Remove `geyser` from path and service names

--- ansible/files/dnf-reboot.sh
+++ ansible/files/dnf-reboot.sh
@@ -2,7 +2,7 @@
 
 dnf check-update
 if [ "$?" = "100" ] ; then
-	systemctl stop minecraft-geyser
+	systemctl stop minecraft
 	dnf -y update
 
 	if [ ! "$(dnf needs-restarting -r)" ] ; then
@@ -12,6 +12,6 @@ if [ "$?" = "100" ] ; then
 		if $SERVICES ; then
 			systemctl restart "$SERVICES"
 		fi
-		systemctl start minecraft-geyser
+		systemctl start minecraft
 	fi
 fi
