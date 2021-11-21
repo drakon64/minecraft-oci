32c2eb8cc194ff663c615b34780e98d612d8c0e8
Adam Chance <adam.chance10@live.co.uk>
2021-11-21 20:32:15 +0000

    Drop old ARM and i386 from Phantom Docker Oracle Linux only supports ARMv8 and AMD64

--- phantom/phantom_docker.sh
+++ phantom/phantom_docker.sh
@@ -6,15 +6,6 @@ RELEASES=$(curl --silent "https://api.github.com/repos/jhead/phantom/releases" |
 if [ "$ARCHITECTURE" = "aarch64" ]
 then
 	curl "$(echo "$RELEASES" | jq -r 'select(.name == "phantom-linux-arm8") | .browser_download_url')" -L -o /phantom/phantom-linux
-elif [ "$ARCHITECTURE" = "armv7l" ]
-then
-	curl "$(echo "$RELEASES" | jq -r 'select(.name == "phantom-linux-arm7") | .browser_download_url')" -L -o /phantom/phantom-linux
-elif [ "$ARCHITECTURE" = "armv6l" ]
-then
-	curl "$(echo "$RELEASES" | jq -r 'select(.name == "phantom-linux-arm6") | .browser_download_url')" -L -o /phantom/phantom-linux
-elif [ "$ARCHITECTURE" = "armv5l" ]
-then
-	curl "$(echo "$RELEASES" | jq -r 'select(.name == "phantom-linux-arm5") | .browser_download_url')" -L -o /phantom/phantom-linux
 elif [ "$ARCHITECTURE" = "x86_64" ]
 then
 	curl "$(echo "$RELEASES" | jq -r 'select(.name == "phantom-linux") | .browser_download_url')" -L -o /phantom/phantom-linux
