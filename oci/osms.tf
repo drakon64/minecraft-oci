4c8796bf350fc6609b1d7fadd395ca0834e71fbc
Adam Chance <adam.chance10@live.co.uk>
2021-12-01 09:46:42 +0000

    Re-enable OSMS

--- oci/osms.tf
+++ oci/osms.tf
@@ -1,10 +1,10 @@
-# resource "oci_identity_policy" "minecraft_osms" {
-# 	compartment_id = var.oci_tenancy
-# 	description = "Minecraft OS Management Service"
-# 	name = "${var.oci_compute_display_name}-osms"
-# 	statements = [
-# 		"Allow service osms to read instances in tenancy",
-# 		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft.name} to read instance-family in compartment id ${oci_identity_compartment.minecraft_compartment.id}",
-# 		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft.name} to use osms-managed-instances in compartment id ${oci_identity_compartment.minecraft_compartment.id}"
-# 	]
-# }
+resource "oci_identity_policy" "minecraft_osms" {
+	compartment_id = var.oci_tenancy
+	description = "Minecraft OS Management Service"
+	name = "${var.oci_compute_display_name}-osms"
+	statements = [
+		"Allow service osms to read instances in tenancy",
+		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft.name} to read instance-family in compartment id ${oci_identity_compartment.minecraft_compartment.id}",
+		"Allow dynamic-group ${oci_identity_dynamic_group.minecraft.name} to use osms-managed-instances in compartment id ${oci_identity_compartment.minecraft_compartment.id}"
+	]
+}
