8039be42db5f16686679a42bdcf2bb0441e30d00
Adam Chance <adam.chance10@live.co.uk>
2021-12-18 11:52:07 +0000

    Remove the high heap utilization alarm

--- oci/monitoring.tf
+++ oci/monitoring.tf
@@ -116,22 +116,6 @@ resource "oci_monitoring_alarm" "Critical-Memory-Utilization" {
 	severity = "CRITICAL"
 }
 
-resource "oci_monitoring_alarm" "High-Heap-Utilization" {
-	compartment_id = oci_identity_compartment.minecraft_compartment.id
-	destinations = [
-		oci_ons_notification_topic.minecraft_monitoring.id
-	]
-	display_name = "High Heap Utilization on ${oci_core_instance.minecraft_instance.display_name}"
-	is_enabled = "true"
-	metric_compartment_id = oci_identity_compartment.minecraft_compartment.id
-	metric_compartment_id_in_subtree = "false"
-	namespace = "minecraft"
-	pending_duration = "PT5M"
-	query = "heapUtilization[5m]{resourceId = ${oci_core_instance.minecraft_instance.id}}.mean() > 75"
-	resolution = "1m"
-	severity = "WARNING"
-}
-
 resource "oci_monitoring_alarm" "Critical-Heap-Utilization" {
 	compartment_id = oci_identity_compartment.minecraft_compartment.id
 	destinations = [
