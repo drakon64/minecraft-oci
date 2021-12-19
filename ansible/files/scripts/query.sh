6b885e6ee18401b687748be93651781320ec3d9f
Adam Chance <adam.chance10@live.co.uk>
2021-12-19 16:26:31 +0000

    Pause Chunky if players are on the server

--- ansible/files/scripts/query.sh
+++ ansible/files/scripts/query.sh
@@ -8,6 +8,13 @@ RESOURCE_ID=$(echo "$METADATA" | jq -r .id)
 
 IP=$(oci --auth instance_principal compute instance list-vnics --instance-id "$RESOURCE_ID" --compartment-id "$COMPARTMENT_ID" | jq -r '.data[]."public-ip"')
 QUERY=$(curl --silent https://api.mcsrvstat.us/2/"$IP" | jq -r .)
+PLAYERS_ONLINE=$(echo "$QUERY" | jq -r .players.online)
+
+if [ "$PLAYERS_ONLINE" -gt 0 ] ; then
+	tmux send-keys -t minecraft chunky SPACE pause ENTER
+elif [ "$PLAYERS_ONLINE" -eq 0 ] ; then
+	tmux send-keys -t minecraft chunky SPACE continue ENTER
+fi
 
 oci --auth instance_principal monitoring metric-data post --metric-data "[
 	{
@@ -15,7 +22,7 @@ oci --auth instance_principal monitoring metric-data post --metric-data "[
 		\"datapoints\": [
 			{
 				\"timestamp\": \"$(date --iso-8601=seconds)\",
-				\"value\": $(echo "$QUERY" | jq -r .players.online)
+				\"value\": \"$PLAYERS_ONLINE\"
 			}
 		],
 		\"dimensions\": {
