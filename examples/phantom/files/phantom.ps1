4e25debb951852c1cadc6689741ecc9547f58b1d
Adam Chance <adam.chance10@live.co.uk>
2021-12-20 13:12:33 +0000

    Improve Phantom scripts

--- examples/phantom/files/phantom.ps1
+++
@@ -1,8 +0,0 @@
-If ([System.Environment]::Is64BitOperatingSystem) {
-	$Phantom = "phantom-windows.exe"
-}
-Else {
-	$Phantom = "phantom-windows-32bit.exe"
-}
-
-Start-Process -FilePath $Phantom -ArgumentList "-server $(Get-Content -Path phantom.txt)" -NoNewWindow -Wait
--- examples/phantom/files/phantom.sh
+++ examples/phantom/files/phantom.sh
@@ -5,28 +5,31 @@ ARCHITECTURE=$(uname -m)
 
 SERVER=$(< phantom.txt)
 
-if [ "$UNAME" = "Darwin" ] && [ "$ARCHITECTURE" = "arm64" ]
-then
-	./phantom-macos-arm8 -server "$SERVER"
-elif [ "$UNAME" = "Darwin" ] && [ "$ARCHITECTURE" = "x86_64" ]
-then
-	./phantom-macos -server "$SERVER"
-elif [ "$UNAME" = "Linux" ] && [ "$ARCHITECTURE" = "aarch64" ]
-then
-	./phantom-linux-arm8 -server "$SERVER"
-elif [ "$UNAME" = "Linux" ] && [ "$ARCHITECTURE" = "armv7l" ]
-then
-	./phantom-linux-arm7 -server "$SERVER"
-elif [ "$UNAME" = "Linux" ] && [ "$ARCHITECTURE" = "armv6l" ]
-then
-	./phantom-linux-arm6 -server "$SERVER"
-elif [ "$UNAME" = "Linux" ] && [ "$ARCHITECTURE" = "armv5l" ]
-then
-	./phantom-linux-arm5 -server "$SERVER"
-elif [ "$UNAME" = "Linux" ] && [ "$ARCHITECTURE" = "x86_64" ]
-then
-	./phantom-linux -server "$SERVER"
+if [ "$UNAME" = "Darwin" ] ; then
+	if [ "$ARCHITECTURE" = "arm64" ] ; then
+		./phantom-macos-arm8 -server "$SERVER"
+	elif [ "$ARCHITECTURE" = "x86_64" ] ; then
+		./phantom-macos -server "$SERVER"
+	else
+		echo "Unknown architecture"
+		exit 1
+	fi
+elif [ "$UNAME" = "Linux" ] ; then
+	if [ "$ARCHITECTURE" = "aarch64" ] ; then
+		./phantom-linux-arm8 -server "$SERVER"
+	elif [ "$ARCHITECTURE" = "armv7l" ] ; then
+		./phantom-linux-arm7 -server "$SERVER"
+	elif [ "$ARCHITECTURE" = "armv6l" ] ; then
+		./phantom-linux-arm6 -server "$SERVER"
+	elif [ "$ARCHITECTURE" = "armv5l" ] ; then
+		./phantom-linux-arm5 -server "$SERVER"
+	elif [ "$ARCHITECTURE" = "x86_64" ] ; then
+		./phantom-linux -server "$SERVER"
+	else
+		echo "Unknown architecture"
+		exit 1
+	fi
 else
-	echo "Unknown uname"
+	echo "Unknown OS"
 	exit 1
 fi
