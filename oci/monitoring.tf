ce830c4d84c888a996ed75fe2448359d03b48f15
Adam Chance <adam.chance10@live.co.uk>
2022-02-05 13:00:19 +0000

    Monitor CPU load average instead of busy time

--- oci/monitoring.tf
+++ oci/monitoring.tf
@@ -1,23 +1,7 @@
 locals {
-  cpu_warn        = var.oci_compute_ocpus > 1 ? 100 - (100 / var.oci_compute_ocpus) : 66
   heap_warn       = (((var.oci_compute_memory - 2) - (1 / 3)) / (var.oci_compute_memory - 2)) * 100
   memory_critical = ((var.oci_compute_memory - (1 / 6)) / var.oci_compute_memory) * 100
   heap_critical   = ((var.oci_compute_memory - (1 / 3)) / var.oci_compute_memory) * 100
-}
-
-resource "oci_monitoring_alarm" "High-CPU-Utilization" {
-  compartment_id = oci_identity_compartment.minecraft_compartment.id
-  destinations = [
-    oci_ons_notification_topic.minecraft_monitoring.id
-  ]
-  display_name                     = "High CPU Utilization on ${oci_core_instance.minecraft_instance.display_name}"
-  is_enabled                       = "true"
-  metric_compartment_id            = oci_identity_compartment.minecraft_compartment.id
-  metric_compartment_id_in_subtree = "false"
-  namespace                        = "oci_computeagent"
-  query                            = "CpuUtilization[5m]{resourceId = ${oci_core_instance.minecraft_instance.id}}.mean() > ${local.cpu_warn}"
-  resolution                       = "1m"
-  severity                         = "WARNING"
 }
 
 resource "oci_monitoring_alarm" "High-Heap-Utilization" {
@@ -78,6 +62,22 @@ resource "oci_monitoring_alarm" "High-Disk-Utilization-boot-efi" {
   query                            = "EfiDiskUtilization[5m]{resourceId = ${oci_core_instance.minecraft_instance.id}}.mean() > 75"
   resolution                       = "1m"
   severity                         = "WARNING"
+}
+
+resource "oci_monitoring_alarm" "Critical-CPU-Load-Average" {
+  compartment_id = oci_identity_compartment.minecraft_compartment.id
+  destinations = [
+    oci_ons_notification_topic.minecraft_monitoring.id
+  ]
+  display_name                     = "Critical CPU Load Average on ${oci_core_instance.minecraft_instance.display_name}"
+  is_enabled                       = "true"
+  metric_compartment_id            = oci_identity_compartment.minecraft_compartment.id
+  metric_compartment_id_in_subtree = "false"
+  namespace                        = "minecraft"
+  query                            = "LoadAverage[5m]{resourceId = ${oci_core_instance.minecraft_instance.id}}.mean() > 100"
+  repeat_notification_duration     = "PT1H"
+  resolution                       = "1m"
+  severity                         = "CRITICAL"
 }
 
 resource "oci_monitoring_alarm" "Critical-Memory-Utilization" {
