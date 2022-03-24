d33b9746aa979c7aa2b5b087749cd851262d62a5
Adam Chance <adam.chance10@live.co.uk>
2022-03-24 20:29:11 +0000

    Fix server being killed when stopping

--- ansible/files/scripts/stop.sh
+++ ansible/files/scripts/stop.sh
@@ -2,7 +2,12 @@
 
 if [ -s "$1"/stop ]
 then
-	tmux send-keys -t minecraft-"$1" kick SPACE @p SPACE "$(< "$1"/stop)" ENTER
+	sudo -u minecraft tmux send-keys -t minecraft-"$1" kick SPACE @p SPACE "$(< "$1"/stop)" ENTER
 fi
 
-tmux send-keys -t minecraft-"$1" stop ENTER
+sudo -u minecraft tmux send-keys -t minecraft-"$1" stop ENTER
+
+while sudo -u minecraft tmux list-sessions | grep -q minecraft-"$1"
+do
+	sleep 1
+done
