0e0e5d3f779de3076a9911f65c7d37cd6cb0f448
Adam Chance <adam.chance10@live.co.uk>
2022-03-29 18:26:32 +0100

    Fix systemd units and scripts

--- ansible/files/scripts/stop.sh
+++ ansible/files/scripts/stop.sh
@@ -2,12 +2,7 @@
 
 if [ -s "$1"/stop ]
 then
-	sudo -u minecraft tmux send-keys -t minecraft-"$1" kick SPACE @p SPACE "$(< "$1"/stop)" ENTER
+	tmux send-keys -t minecraft-"$1" kick SPACE @p SPACE "$(< "$1"/stop)" ENTER
 fi
 
-sudo -u minecraft tmux send-keys -t minecraft-"$1" stop ENTER
-
-while sudo -u minecraft tmux list-sessions | grep -q minecraft-"$1"
-do
-	sleep 1
-done
+tmux send-keys -t minecraft-"$1" stop ENTER
